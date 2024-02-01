a7378638d14abb8948a298605ed19d5ac2f9b054
arpegx <arpegx@posteo.de>
2024-02-01 01:54:15 +0100

    plant image

--- database/factories/PlantFactory.php
+++ database/factories/PlantFactory.php
@@ -18,6 +18,7 @@ class PlantFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'image' => 'resources/assets/images/calathea_korbmarante.jpeg',
             'watered_at' => fake()->date(),
         ];
     }
--- resources/views/livewire/plant/plant-row.blade.php
+++ resources/views/livewire/plant/plant-row.blade.php
@@ -1,4 +1,6 @@
 <div>
+    {{-- <img src="{{url($plant->image)}}" alt="{{__('Image of the Plant')}}"> --}}
+    <img src="{{ Vite::asset($plant->image) }}" class="rounded h-10 w-10">
     <p>{{$plant->name}}</p>
     <p>{{$plant->watered_at}}</p>
 </div>
