36379aa4a4ca01128f59571924d3a7df7ac69b62
arpegx <arpegx@posteo.de>
2024-02-01 01:10:47 +0100

    vite in livewire default layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,8 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
         <title>{{ $title ?? 'Page Title' }}</title>
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         {{ $slot }}
--- resources/views/livewire/plant/plant-table.blade.php
+++ resources/views/livewire/plant/plant-table.blade.php
@@ -1,3 +1,4 @@
 <div>
+    <h1>Plant</h1>
     The whole world belongs to you.
 </div>
