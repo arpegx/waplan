0358888cc73e3b01b8007ebf5c2034ff24537f82
arpegx <arpegx@posteo.de>
2024-02-02 21:21:25 +0100

    production behalf

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,18 +4,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href="{{asset('assets/styles/build.css')}}">
         
         <title>{{ $title ?? 'Page Title' }}</title>
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
+        <link rel="stylesheet" href="{{asset('assets/styles/build.css')}}">
     </head>
-    {{-- bg-[url('http://192.168.0.22:4080/assets/images/calathea_korbmarante.jpeg')] bg-no-repeat bg-cover --}}
     <body class="flex h-screen items-center justify-center"
         style="background-image: url('assets/images/calathea_korbmarante.jpeg');
             background-repeat: no-repeat;
-            background-size: cover;">
+            background-size: cover;"
+        > 
         <div class="rounded container mx-auto w-4/5 bg-white h-5/6">
             {{ $slot }}
         </div>
--- resources/views/livewire/plant/plant-table.blade.php
+++ resources/views/livewire/plant/plant-table.blade.php
@@ -1,11 +1,11 @@
 <div>
     <h1 class="text-3xl text-center font-serif my-2">{{__('Plants')}}</h1>
-    <div class="h-[38rem] overflow-auto">
+    <div class="h-[36rem] overflow-auto">
         @foreach ($plants as $plant)
             @livewire('plant.plant-row', ['plant' => $plant], key($plant->id))
         @endforeach
     </div>
-    <div class="w-fit mx-auto h-1 flex items-center justify-center">
+    <div class="w-fit mx-auto pt-5 flex items-center justify-center">
         {{$plants->links()}}
     </div>
 </div>
