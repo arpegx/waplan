666a14afb2681f53894380f1fd7d71b215395702
arpegx <arpegx@posteo.de>
2024-02-03 15:02:04 +0100

    watered_at in seperate div for easier scroll

--- resources/views/livewire/plant/plant-row.blade.php
+++ resources/views/livewire/plant/plant-row.blade.php
@@ -1,15 +1,16 @@
-<div class="flex items-center m-1 w-95 border hover:bg-slate-100" >
+<div class="flex items-center m-1 w-95 border hover:bg-slate-100">
     <div class="mx-1">
         <img src="{{ asset('assets/images/calathea_korbmarante.jpeg') }}" alt="{{__('Image of the Plant')}}" title="{{__('Image of the Plant')}}" class="rounded h-10 w-10">
     </div>
     {{-- wire:click -> for desktop --}}
-    <div @if($selected) wire:touchstart.prevent="$parent.remove({{$plant->id}})" 
+    <div @class(['grow focus:bg-white', 'bg-teal-50' => $selected])
+         @if($selected) wire:touchstart.prevent="$parent.remove({{$plant->id}})" 
          @else wire:touchstart.prevent="$parent.add({{$plant->id}})" @endif
-         @class(['grow focus:bg-white', 'bg-teal-50' => $selected])
-         >
+        >
         <p class="leading-4">{{$plant->name}}</p>
         <p class="text-xs leading-4"><i>{{$plant->botanical}}</i></p>
+    </div>
+    <div>        
         <p class="text-right leading-4 me-1">{{date('d.m.Y', strtotime($plant->watered_at))}}</p>
-        @error('plant') <em>{{$message}}</em>  @enderror
     </div>
 </div>
